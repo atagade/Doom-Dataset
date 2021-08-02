8db4a8596c04f191c94851a1e6405abe126bb44f
atagade <atagade19@gmail.com>
2021-08-02 14:43:44 +0530

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt install cmake libboost-all-dev libsdl2-dev libfreetype6-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libjpeg-dev libbz2-dev libfluidsynth-dev libgme-dev libopenal-dev zlib1g-dev timidity tar nasm
+pip install vizdoom
+git clone https://github.com/glample/Arnold
+cd Arnold
